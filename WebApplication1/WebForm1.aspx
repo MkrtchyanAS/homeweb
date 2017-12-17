4e666f687e95abedaae4554d8cf44ad1c8c0de09
Армен Мкртчян <MkrtchyanAS@outlook.com>
2017-12-17 23:29:29 +0500

    Добавьте файлы проекта.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .position {
+            text-align: center;
+            background-color: #FFCC00;
+            margin-left: 157px;
+        }
+        .auto-style1 {
+            text-align: left;
+            font-size: small;
+        }
+        </style>
+</head>
+<body style="height: 286px">
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <p class="auto-style1">
+            &nbsp;</p>
+        <p style="margin-left: 312px">
+            <asp:Label ID="LabelName" runat="server" Text="Имя"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged" style="margin-left: 45px" Width="217px"></asp:TextBox>
+        </p>
+        <p style="margin-left: 297px">
+            <asp:Label ID="LabelSurname" runat="server" Text="Фамилия"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged" style="margin-left: 16px" Width="214px"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p style="width: 563px; margin-left: 156px">
+            <asp:Label ID="LabelWelcome" runat="server" Text="Введите ваше имя..." ForeColor="Red"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" CssClass ="position" Text="Отправить" BorderStyle="Solid" Width="252px" />
+            <asp:Button ID="Button2" runat="server" BackColor="#FF9900" BorderColor="#FFCC00" BorderStyle="Solid" OnClick="Button2_Click" style="margin-left: 175px" Text="Восстановить" Width="204px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string sname = string.Empty;
+            LabelWelcome.Text = "Здравствуйте, " + TextBox1.Text + " " + TextBox2.Text + "!" + " " + " Добро пожаловать!";
+            LabelWelcome.ForeColor = System.Drawing.Color.DarkRed;
+            ViewState["FirstName"] = TextBox1.Text;
+            ViewState["LastName"] = TextBox2.Text;
+            //SaveTextInTextBox(TextBox1.Controls, true);
+            //SaveTextInTextBox(TextBox2.Controls, true);
+            //TextBox1.Text = " ";
+            //TextBox2.Text = " ";
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string a = TextBox1.Text;
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = (string)ViewState["FirstName"];
+            TextBox2.Text = (string)ViewState["LastName"];
+            //RestoreTextToTextBox(TextBox1.Controls, true);
+            //RestoreTextToTextBox(TextBox2.Controls, true);
+
+        }
+
+        private void SaveTextInTextBox(ControlCollection controls, bool SaveNested)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox)
+                {
+                    ViewState[control.ID] = ((TextBox)control).Text;
+                }
+
+                //if (control.Controls != null && SaveNested)
+                //{
+                  //  SaveTextInTextBox(control.Controls, true);
+                //}
+            }
+
+        }
+
+        private void RestoreTextToTextBox(ControlCollection controls, bool SaveNested)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox)
+                {
+                    if (ViewState[control.ID] != null)
+                    {
+                        ((TextBox)control).Text = (string)ViewState[control.ID];
+
+                    }
+                    /*if (control.Controls != null && SaveNested)
+                    {
+
+                        RestoreTextToTextBox(control.Controls, true);
+                    }*/
+                }
+
+            }
+
+        }
+    }
+}
